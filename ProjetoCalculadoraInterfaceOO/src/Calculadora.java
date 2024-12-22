fd6b06df864c9ac3257bd67110f172bbf2cdba75
Lucas Santos <100425690+lucassantos540@users.noreply.github.com>
2024-12-22 19:18:54 -0300

    Pasta do projeto

---
+++ ProjetoCalculadoraInterfaceOO/src/Calculadora.java
@@ -0,0 +1,23 @@
+public class Calculadora {
+    
+    public double numero1;
+    public double numero2;
+    
+    public double Somar(){
+        return this.numero1+this.numero2;
+    }
+    
+    public double Subtrair(){
+        return this.numero1-this.numero2;
+    }
+    
+    public double Multiplicar(){
+        return this.numero1*this.numero2;
+    }
+    
+    public double Dividir(){
+        return this.numero1/this.numero2;
+    }
+    
+    
+}
